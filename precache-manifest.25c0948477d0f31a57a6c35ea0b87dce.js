a246f8956551ceef8a62fc96b692d8b8a9de941e
ByeongRyul Cho <ryullulala@icloud.com>
2022-04-10 18:49:07 +0900

    Updates

---
+++ precache-manifest.25c0948477d0f31a57a6c35ea0b87dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35efe39f81e698568c60",
+    "url": "/react-movie-app/static/css/main.ea199b63.chunk.css"
+  },
+  {
+    "revision": "35efe39f81e698568c60",
+    "url": "/react-movie-app/static/js/main.35efe39f.chunk.js"
+  },
+  {
+    "revision": "34e8ad9cceb35c9e50fc",
+    "url": "/react-movie-app/static/js/1.34e8ad9c.chunk.js"
+  },
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "902ee294590565c6bb24f8c42827824b",
+    "url": "/react-movie-app/index.html"
+  }
+];
